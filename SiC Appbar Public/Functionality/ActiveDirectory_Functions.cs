c94094f935fb3830ef0770929e9d88be301e8b06
boncz92 <jgbonc@gmail.com>
2020-06-30 11:51:09 -0400

    Add files via upload

---
+++ SiC Appbar Public/Functionality/ActiveDirectory_Functions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiC_Appbar
+{
+    class ActiveDirectory_Functions
+    {
+        private void Test()
+        {
+
+        }
+    }
+
+}
